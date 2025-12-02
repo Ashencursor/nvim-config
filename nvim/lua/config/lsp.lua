2e4386017768a3bb2e85bc1429b1d865eff88a4f
Owen3775 <98483487+Owen3775@users.noreply.github.com>
2025-12-02 18:14:04 -0500

    added new version of lsp for both cpp and rust

---
+++ nvim/lua/config/lsp.lua
@@ -0,0 +1,57 @@
+-- Capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+pcall(function()
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+end)
+
+-- Keymaps to all servers
+local function on_attach(_, bufnr)
+    local opts = { buffer = bufnr }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+    end, opts)
+end
+
+-- defaults to all servers
+vim.lsp.config("*", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+-- Rust
+vim.lsp.config("rust_analyzer", {
+    filetypes = { "rust" },
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            check = { command = "clippy" },
+        }
+    }
+})
+
+-- C++
+vim.lsp.config("clangd", {
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--completion-style=detailed",
+        "--header-insertion=iwyu",
+    },
+})
+
+-- Start the LSPs
+-- Cant do vim.lsp.enable() for some reason
+-- I think its because it cant detect the stuff
+vim.lsp.enable({       
+    "clangd",
+    "rust_analyzer",
+    })
+
+
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,82 +1,12 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
+    "neovim/nvim-lspconfig",
     dependencies = {
-      "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
     },
-
     config = function()
-      local lsp = require("lsp-zero")
-
-      lsp.preset("recommended")
-
-      -- ======================================
-      -- 1. Mason MUST be set up first
-      -- ======================================
       require("mason").setup()
-
-      -- ======================================
-      -- 2. Mason-LSPConfig handles server setup
-      -- ======================================
-      require("mason-lspconfig").setup({
-        ensure_installed = { "clangd" },
-
-        handlers = {
-          -- Default handler for all servers
-          function(server)
-            require("lspconfig")[server].setup({})
-          end,
-
-          -- Custom clangd config
-          ["clangd"] = function()
-            require("lspconfig").clangd.setup({
-              cmd = { "clangd", "--background-index", "--clang-tidy" },
-              filetypes = { "c", "cpp", "objc", "objcpp" },
-            })
-          end,
-        },
-      })
-
-      -- ======================================
-      -- CMP SETUP (new for lsp-zero v3)
-      -- ======================================
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }
-      })
-      
-      lsp.on_attach(function(_, bufnr)
-        local opts = { buffer = bufnr }
-        local km = vim.keymap.set
-
-        km("n", "gd", vim.lsp.buf.definition, opts)
-        km("n", "gr", vim.lsp.buf.references, opts)
-        km("n", "K", vim.lsp.buf.hover, opts)
-        km("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        km("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      end)
-
-      lsp.setup()
-
-      vim.diagnostic.config({
-        virtual_text = true,
-      })
+      require("config.lsp")   -- <-- loads the whole folder/file
     end,
-  }
+  },
 }
